a2ea887806e0dbb139406f60c4d2fd05735288c5
rockclimber112358 <rockclimber112358@gmail.com>
2014-06-26 15:53:32 -0600

    Update plotGlm.R

--- plotGlm.R
+++ plotGlm.R
@@ -1,14 +1,14 @@
 plotGlm = function(fit){
   if( !is(fit, "glm") )
     stop("fit must be a glm object!")
-  if( !fit$family[1] %in% c("gaussian", "binomial") )
-    warning("Error dist. isn't gaussian or binomial, raw coeff CI's returned.")
   
   library(ggplot2)
   library(reshape)
   
   coeff = data.frame( summary(fit)$coeff)
   colnames(coeff) = c("Estimate", "Std.Error", "t.value", "p.value")
+  coeff$Hi = coeff$Estimate + 2*coeff$Std.Error
+  coeff$Lo = coeff$Estimate - 2*coeff$Std.Error
   coeff$variable = rownames(coeff)
   coeff$groupVariable = coeff$variable #changed later for factor variables
   coeff$groupEst = coeff$Estimate #changed later for factor variables
@@ -30,18 +30,12 @@ plotGlm = function(fit){
                                ,ifelse(coeff$p.value<0.01, "**"
                                ,ifelse(coeff$p.value<0.05, "*"
                                ,ifelse(coeff$p.value<.1,".","")))))
-  coeff$Hi = coeff$Estimate + 2*coeff$Std.Error
-  coeff$Lo = coeff$Estimate - 2*coeff$Std.Error
-  if(fit$family[1]=="binomial"){
-    coeff$Hi = exp(coeff$Hi)
-    coeff$Lo = exp(coeff$Lo)
-  }
-  yLab = ifelse(fit$family[1]=="binomial", "Odds Ratio", "Coefficient")
+  yLab = ifelse(fit$family[1]=="binomial", "Log(Odds Ratio)", "Coefficient")
 
   p = ggplot(coeff, aes(x=variable) ) +
     geom_bar(aes(y=Estimate, fill=Estimate>0), stat="identity", width=.4) +
     geom_errorbar(aes(ymax=Hi, ymin=Lo), width=.2) +
-    geom_text(aes(x=variable, y=-.25, label=lab ), vjust=-2, hjust=0 ) +
+    geom_text(aes(x=variable, y=-.25, label=lab ), vjust=-2, hjust=0, size=3) +
     coord_flip() + guides(fill=F) + labs(x="", y=yLab)
   
   return(p)
