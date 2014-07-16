eade7af4496dd33b05d616b758f2bebcc10bbb3f
rockclimber112358 <rockclimber112358@gmail.com>
2014-07-16 08:34:45 -0600

    Update geneticAlgorithm.R

--- geneticAlgorithm.R
+++ geneticAlgorithm.R
@@ -18,6 +18,7 @@
 #popSize: Size of the population to maintain for each generation.
 #nGen: Number of generations to create in the algorithm.  More generations means longer
 #  computation time but a higher chance of converging to the global optimum.
+#verbose: Should the best parameter be output after each run?
 GA = function( creater, fitness, crossover, breedProp=1/2, mutateProb=.05, nKeepTop=3
   ,check=function(listEl){return(TRUE)}
   ,mutation=function(listEl){return(creater())}
@@ -29,7 +30,20 @@ GA = function( creater, fitness, crossover, breedProp=1/2, mutateProb=.05, nKeep
     pSel = fitnessList/sum(fitnessList)
     selectPop = sample(listList, size=n, prob=pSel, replace=TRUE)
   }
-  ,popSize=10, nGen=100, ...){
+  ,popSize=10, nGen=100, verbose=T, ...){
+  
+  if(popSize<1)
+    stop("popSize must be >=1!")
+  popSize = round(popSize)
+  if(nGen<1)
+    stop("nGen must be >=1!")
+  nGen = round(nGen)
+  if(breedProp<0 | breedProp > 1)
+    stop("breedProp must be in [0,1]!")
+  if(mutateProb<0 | mutateProb>1)
+    stop("breedProp must be in [0,1]!")
+  if(nKeepTop<0 | nKeepTop > popSize)
+    stop("nKeepTop must be in [0,popSize]!")
   
   #Simple function check
   fitness(creater())
@@ -38,6 +52,8 @@ GA = function( creater, fitness, crossover, breedProp=1/2, mutateProb=.05, nKeep
   print("Crossover function evaluated successfully!")
   mutation(creater())
   print("Mutation function evaluated successfully!")
+  check(creater())
+  print("Check function evaluated successfully!")
 
   #Initialize population
   listList = lapply(1:popSize, function(i){creater()})
@@ -52,23 +68,33 @@ GA = function( creater, fitness, crossover, breedProp=1/2, mutateProb=.05, nKeep
     #Select new individuals from population.  Note that selection() is called once 
     #per loop so that we only evaluate fitness functions once per loop
     fitnessList = do.call("c",lapply(listList, fitness))
-    cat("Best fitness is",max(fitnessList),"occuring for element:\n")
-    print(listList[which.max(fitnessList)])
+    if(verbose){
+      cat("Best fitness is",max(fitnessList),"occuring for element:\n")
+      print(listList[which.max(fitnessList)])
+    }
 
     selList = selection(listList, fitnessList, nSelect)
+    if(!all(do.call("c", lapply(selList, check)) ) )
+      stop("Selection produced invalid parameters!")
     
     #Crossover parents
     newList = list()
     for(j in 1:nCrossover) #for loop ok since this computation should be minimal
       newList[[j]] = crossover(selList[[2*j-1]],selList[[2*j]])
+    if(!all(do.call("c", lapply(newList, check)) ) )
+      stop("Crossover produced invalid parameters!")
         
     #Keep survivors
     for(j in 1:nSurvive)
       newList[[j+nCrossover]] = selList[[2*nCrossover+j]]
+    if(!all(do.call("c", lapply(newList[(nCrossover+1):nSurvive], check)) ) )
+      stop("Survivors are invalid!")
     
     #Mutate randomly selected obs
     toMutate = rbinom(popSize, size=1, prob=mutateProb)
     newList[toMutate] = lapply(newList[toMutate], mutation)
+    if(!all(do.call("c", lapply(newList[toMutate], check)) ) )
+      stop("Mutations produced invalid parameters!")
     
     #Keep some of the best
     if(nKeepTop>0){
@@ -106,4 +132,5 @@ example = function(){
     listEl[[sample(1:3,size=1)]] = round(runif(1,.5,10.5))
     return(listEl)
   }
+  #GA(creater = creater,fitness = fitness, crossover = crossover, mutation=mutation, nKeepTop=1 )
 }
